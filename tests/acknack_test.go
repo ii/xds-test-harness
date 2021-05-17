ff3904d56803de47e98632f8674ecfadb08c0cac
Zach Mandeville <zz@ii.coop>
2021-05-17 23:01:08 +1200

    move through basic acknack feature PoC

--- tests/acknack_test.go
+++ tests/acknack_test.go
@@ -3,11 +3,16 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"reflect"
 	"time"
 
 	"github.com/cucumber/godog"
+	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	cluster_service "github.com/envoyproxy/go-control-plane/envoy/service/cluster/v3"
+	envoy_service_discovery_v3 "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
+	"github.com/gogo/protobuf/jsonpb"
 	"google.golang.org/grpc"
 
 	pb "github.com/zachmandeville/tester-prototype/api/adapter"
@@ -22,10 +27,10 @@ type runner struct {
 	adapter *grpc.ClientConn
 	target  *grpc.ClientConn
 	shim    *grpc.ClientConn
+	discoveryResponse *envoy_service_discovery_v3.DiscoveryResponse
 }
 
 func (r *runner) aTargetWithClustersSpecifiedWithYaml(yml *godog.DocString) error {
-
 	var specifiedClusters []string
 	spec, err := parser.ParseYaml(yml.Content)
 	if err != nil {
@@ -57,8 +62,7 @@ func (r *runner) aTargetWithClustersSpecifiedWithYaml(yml *godog.DocString) erro
 	for key, _ := range clusters.(map[string]interface{}) {
 		snapshotClusters = append(snapshotClusters, key)
 	}
-	eq := reflect.DeepEqual(snapshotClusters, specifiedClusters)
-	if eq == false {
+	if !reflect.DeepEqual(snapshotClusters, specifiedClusters) {
 		return err
 	}
 	return nil
@@ -97,12 +101,53 @@ func (r *runner) anAdapterLocatedAt(port string) error {
 	return nil
 }
 
-func iGetAResponseContaining(arg1 string) error {
+func (r *runner) iGetAResponseContaining(arg1 string) error {
+	m := jsonpb.Marshaler{}
+    result, _ := m.MarshalToString(r.discoveryResponse)
+	log.Printf("\n\n\n%v\n\n\n\n", result)
 	return godog.ErrPending
 }
 
-func iSendAWildcardRequestToTheCDS() error {
-	return godog.ErrPending
+func (r *runner) iGetADiscoveryResponseMatchingJson(arg1 *godog.DocString) error {
+	var expected, actual interface{}
+	m := jsonpb.Marshaler{}
+    result, _ := m.MarshalToString(r.discoveryResponse)
+	if err := json.Unmarshal([]byte(result), &actual); err != nil {
+		return err
+	}
+	if err := json.Unmarshal([]byte(arg1.Content), &expected); err != nil {
+		return err
+	}
+	if !reflect.DeepEqual(expected, actual) {
+		return fmt.Errorf("expected JSON does not match actual, %v vs. %v", expected, actual)
+	}
+	return nil
+}
+
+
+func (r *runner) iSendADiscoveryRequestMatchingYaml(arg1 *godog.DocString) error {
+	drdata, err := parser.ParseDiscoveryRequest(arg1.Content)
+	if err != nil{
+	  log.Printf("error parsing discovery request: %v\n", err)
+	}
+	dreq := &envoy_service_discovery_v3.DiscoveryRequest{
+		VersionInfo: drdata.VersionInfo,
+		Node: &envoy_config_core_v3.Node{
+			Id: drdata.Node.ID,
+		},
+		ResourceNames: drdata.ResourceNames,
+		TypeUrl:       drdata.TypeURL,
+		ResponseNonce: drdata.ResponseNonce,
+	}
+	c := cluster_service.NewClusterDiscoveryServiceClient(r.target)
+	// TODO this should be a general fetch, not just to clusters.
+	dres, err := c.FetchClusters(context.Background(),dreq)
+	if err != nil {
+		log.Printf("err fetching clusters: %v", err.Error())
+		return err
+	}
+	r.discoveryResponse = dres
+	return nil
 }
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
@@ -116,6 +161,6 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 	ctx.Step(`^a Shim located at "([^"]*)"$`, runner.aShimLocatedAt)
 	ctx.Step(`^a Target located at "([^"]*)"$`, runner.aTargetLocatedAt)
 	ctx.Step(`^an Adapter located at "([^"]*)"$`, runner.anAdapterLocatedAt)
-	ctx.Step(`^I get a response containing "([^"]*)"\.$`, iGetAResponseContaining)
-	ctx.Step(`^I send a wildcard request to the CDS$`, iSendAWildcardRequestToTheCDS)
+	ctx.Step(`^I send a discovery request matching yaml:$`, runner.iSendADiscoveryRequestMatchingYaml)
+    ctx.Step(`^I get a discovery response matching json:$`, runner.iGetADiscoveryResponseMatchingJson)
 }
