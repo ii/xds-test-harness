228ce602de79784c35bf259b1f1a83fbb023457d
Zach Mandeville <zz@ii.coop>
2021-05-27 14:12:15 +1200

    update call to target

--- acknack_test.go
+++ acknack_test.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	// "context"
-	// "encoding/json"
+	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	// "log"
-	// "reflect"
+	"log"
+	"reflect"
 	"time"
+
 	"github.com/cucumber/godog"
-	// envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
-	// cluster_service "github.com/envoyproxy/go-control-plane/envoy/service/cluster/v3"
+	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	cluster_service "github.com/envoyproxy/go-control-plane/envoy/service/cluster/v3"
 	envoy_service_discovery_v3 "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
-	// "github.com/gogo/protobuf/jsonpb"
+	"github.com/gogo/protobuf/jsonpb"
 	"google.golang.org/grpc"
-	// pb "github.com/zachmandeville/tester-prototype/api/adapter"
+
+	pb "github.com/zachmandeville/tester-prototype/api/adapter"
 	"github.com/zachmandeville/tester-prototype/internal/parser"
 )
 
@@ -35,7 +37,7 @@ type Runner struct {
 	DiscoveryResponse *envoy_service_discovery_v3.DiscoveryResponse
 }
 
-func (r *Runner) addPorts(*godog.Scenario)  {
+func (r *Runner) addPorts(*godog.Scenario) {
 	yamlFile, err := ioutil.ReadFile(configFile)
 	if err != nil {
 		fmt.Printf("Error reading setup file: %v", err)
@@ -52,45 +54,71 @@ func (r *Runner) addPorts(*godog.Scenario)  {
 	}
 }
 
-func aTargetSetupWithSnapshotMatchingYaml(snapYaml *godog.DocString) error {
-	_, err := parser.YamlToSnapshot(snapYaml.Content)
+func (r *Runner) aTargetSetupWithSnapshotMatchingYaml(snapYaml *godog.DocString) error {
+	snapshot, err := parser.YamlToSnapshot(snapYaml.Content)
 	if err != nil {
-		fmt.Printf("Error parsing snapshot yaml: %v", err)
+		err = fmt.Errorf("Error parsing snapshot yaml: %v", err)
+		return err
 	}
-	// snapshot := &pb.Snapshot{
-	// 	Version:   "",
-	// 	Endpoints: &pb.Endpoints{
-	// 		Items: []*pb.Endpoints_Endpoint{
-	// 			&pb.Endpoints_Endpoint{
-	// 				Name: "butts",
-	// 			},
-	// 		},
-	// 	},
-		// Clusters:  &pb.Clusters{},
-		// Routes:    &pb.Routes{},
-		// Listeners: &pb.Listeners{},
-		// Runtimes:  &pb.Runtimes{},
-		// Secrets:   &pb.Secrets{},
-	// }
-	return godog.ErrPending
+
+	c := pb.NewAdapterClient(r.Adapter.Conn)
+	_, err = c.SetState(context.Background(), snapshot)
+	if err != nil {
+		err = fmt.Errorf("Cannot Set Target with State: %v\n", err)
+		return err
+	}
+	return nil
 }
 
-func iGetADiscoveryResponseMatchingJson(arg1 *godog.DocString) error {
-	return godog.ErrPending
+func (r *Runner) iSendADiscoveryRequestMatchingYaml(dryaml *godog.DocString) error {
+	drdata, err := parser.ParseDiscoveryRequest(dryaml.Content)
+	if err != nil {
+		err = fmt.Errorf("error parsing discovery request: %v\n", err)
+		return err
+	}
+	dreq := &envoy_service_discovery_v3.DiscoveryRequest{
+		VersionInfo: drdata.VersionInfo,
+		Node: &envoy_config_core_v3.Node{
+			Id: drdata.Node.ID,
+		},
+		ResourceNames: drdata.ResourceNames,
+		TypeUrl:       drdata.TypeURL,
+		ResponseNonce: drdata.ResponseNonce,
+	}
+	c := cluster_service.NewClusterDiscoveryServiceClient(r.Target.Conn)
+	dres, err := c.FetchClusters(context.Background(), dreq)
+	if err != nil {
+		log.Printf("err fetching clusters: %v", err.Error())
+		return err
+	}
+	r.DiscoveryResponse = dres
+	return nil
 }
 
-func iSendADiscoveryRequestMatchingYaml(arg1 *godog.DocString) error {
-	return godog.ErrPending
+func (r *Runner) iGetADiscoveryResponseMatchingJson(arg1 *godog.DocString) error {
+	var expected, actual interface{}
+	m := jsonpb.Marshaler{}
+	result, _ := m.MarshalToString(r.DiscoveryResponse)
+	if err := json.Unmarshal([]byte(result), &actual); err != nil {
+		return err
+	}
+	if err := json.Unmarshal([]byte(arg1.Content), &expected); err != nil {
+		return err
+	}
+	if !reflect.DeepEqual(expected, actual) {
+		return fmt.Errorf("expected JSON does not match actual, %v vs. %v", expected, actual)
+	}
+	return nil
 }
 
-func connectViaGRPC (client *ClientConfig, server string) error {
-		conn, err := grpc.Dial(client.Port, opts...)
-		if err != nil {
-			err = fmt.Errorf("Cannot connect to %v: %v", server, err)
-			return err
-		}
-		client.Conn = conn
-		return nil
+func connectViaGRPC(client *ClientConfig, server string) error {
+	conn, err := grpc.Dial(client.Port, opts...)
+	if err != nil {
+		err = fmt.Errorf("Cannot connect to %v: %v", server, err)
+		return err
+	}
+	client.Conn = conn
+	return nil
 }
 
 func (r *Runner) isReachableViaGrpc(server string) error {
@@ -106,15 +134,14 @@ func (r *Runner) isReachableViaGrpc(server string) error {
 	}
 }
 
-
 func InitializeScenario(ctx *godog.ScenarioContext) {
 	runner := &Runner{}
 	ctx.BeforeScenario(func(s *godog.Scenario) {
 		opts = append(opts, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(time.Second*5))
 	})
 	ctx.BeforeScenario(runner.addPorts)
-	ctx.Step(`^a Target setup with snapshot matching yaml:$`, aTargetSetupWithSnapshotMatchingYaml)
-	ctx.Step(`^I get a discovery response matching json:$`, iGetADiscoveryResponseMatchingJson)
-	ctx.Step(`^I send a discovery request matching yaml:$`, iSendADiscoveryRequestMatchingYaml)
+	ctx.Step(`^a Target setup with snapshot matching yaml:$`, runner.aTargetSetupWithSnapshotMatchingYaml)
+	ctx.Step(`^I get a discovery response matching json:$`, runner.iGetADiscoveryResponseMatchingJson)
+	ctx.Step(`^I send a discovery request matching yaml:$`, runner.iSendADiscoveryRequestMatchingYaml)
 	ctx.Step(`^"([^"]*)" is reachable via grpc$`, runner.isReachableViaGrpc)
 }
