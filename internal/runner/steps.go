bf4cb0fa95913a8855e203933a94ad97cd253549
Zach Mandeville <zz@ii.coop>
2021-09-28 10:51:50 +1300

    Remove old, obsolete code
    Note: this commit means the tests in the resource.feature file will no longer
    work, but they are being ported over to the new style which is a much
    more pleasant test writing/reading experience.

--- internal/runner/steps.go
+++ internal/runner/steps.go
@@ -46,57 +46,6 @@ func clustersMatch(expected *pb.Snapshot, actual *parser.DiscoveryResponse) bool
 	return sortCompare(expectedClusters, actualClusters)
 }
 
-
-func (r *Runner) ATargetSetupWithTheFollowingState(state *godog.DocString) error {
-	snapshot, err := parser.YamlToSnapshot(r.NodeID, state.Content)
-	if err != nil {
-		msg := "Could not parse given state to adapter snapshot"
-		log.Error().
-			Stack().
-			Err(err).
-			Msg(msg)
-		return errors.New(msg)
-	}
-	c := pb.NewAdapterClient(r.Adapter.Conn)
-	_, err = c.SetState(context.Background(), snapshot)
-	if err != nil {
-		msg := "Cannot set target with given state"
-		log.Error().
-			Stack().
-			Err(err).
-			Msg(msg)
-		return errors.New(msg)
-	}
-	r.Cache.StartState = snapshot
-	return err
-}
-
-func (r *Runner) TheTargetIsUpdatedToTheFollowingState(state *godog.DocString) error {
-	log.Debug().
-		Msg("Updating target state")
-	snapshot, err := parser.YamlToSnapshot(r.NodeID, state.Content)
-	if err != nil {
-		msg := "Could not parse given state to adapter snapshot"
-		log.Error().
-			Stack().
-			Err(err).
-			Msg(msg)
-		return errors.New(msg)
-	}
-	c := pb.NewAdapterClient(r.Adapter.Conn)
-	_, err = c.SetState(context.Background(), snapshot)
-	if err != nil {
-		msg := "Cannot set target with given state"
-		log.Error().
-			Stack().
-			Err(err).
-			Msg(msg)
-		return errors.New(msg)
-	}
-	r.Cache.StartState = snapshot
-	return err
-}
-
 func (r *Runner) ClientSubscribesToWildcardCDS() error {
 	r.CDS.Req = make(chan *discovery.DiscoveryRequest, 1)
 	r.CDS.Res = make(chan *discovery.DiscoveryResponse, 1)
@@ -125,64 +74,6 @@ func (r *Runner) ClientSubscribesToWildcardLDS() {
 
 	go r.LDSStream()
 	go r.Ack(request, r.LDS)
-}
-
-func (r *Runner) TheClientSubscribesToTheFollowingResources(resources *godog.DocString) error {
-	resourceList, err := parser.ParseResourceList(resources.Content)
-	if err != nil {
-		log.Err(err).Msg("couldn't parse resource list")
-	}
-	r.CDS.Req = make(chan *discovery.DiscoveryRequest, 1)
-	r.CDS.Res = make(chan *discovery.DiscoveryResponse, 1)
-	r.CDS.Err = make(chan error, 1)
-	r.CDS.Done = make(chan bool, 1)
-	r.CDS.Cache.InitResource = resourceList
-
-	request := r.NewCDSRequest(r.CDS.Cache.InitResource)
-
-	go r.CDSStream()
-	go r.Ack(request, r.CDS)
-	return nil
-}
-
-func (r *Runner) ClientReceivesTheFollowingVersionAndClustersAlongWithNonce(resources *godog.DocString) error {
-	expected, err := parser.YamlToSnapshot(r.NodeID, resources.Content)
-	if err != nil {
-		msg := "Couldn't parse test yaml. This is a problem with the test, not the target."
-		log.Err(err).
-			Msg(msg)
-		return errors.New(msg)
-	}
-
-	for {
-		select {
-		case <-time.After(6 * time.Second):
-			err := errors.New("Correct response not found after grace period of 6 seconds")
-			log.Err(err).
-				Msg("")
-			return err
-		case err := <- r.CDS.Err:
-			log.Err(err).Msg("From our step")
-			return errors.New("Could not find expected response within stream's context deadline")
-		default:
-			if len(r.CDS.Cache.Responses) > 0 {
-				for _, response := range r.CDS.Cache.Responses {
-					actual, err := parser.ParseDiscoveryResponse(response)
-					if err != nil {
-						msg := "Could not parse Cached Response"
-						log.Err(err).
-							Msg(msg)
-						return errors.New(msg)
-					}
-					if versionsMatch(expected, actual) && clustersMatch(expected, actual) {
-						log.Debug().
-							Msgf("Found Expected Response.\nexpected:%v\nactual: %v\n", expected, actual)
-						return nil
-					}
-				}
-			}
-		}
-	}
 }
 
 func (r *Runner) TheClientSendsAnACKToWhichTheServerDoesNotRespond() error {
@@ -300,12 +191,6 @@ func (r *Runner) TheClientSendsAnACKToWhichTheDoesNotRespond(service string) err
 
 
 func (r *Runner) LoadSteps(ctx *godog.ScenarioContext) {
-	ctx.Step(`^a target setup with the following state:$`, r.ATargetSetupWithTheFollowingState)
-	ctx.Step(`^the Client subscribes to wildcard CDS$`, r.ClientSubscribesToWildcardCDS)
-    ctx.Step(`^the Client subscribes to the following resources:$`, r.TheClientSubscribesToTheFollowingResources)
-	ctx.Step(`^the Client receives the following version and clusters, along with a nonce:$`, r.ClientReceivesTheFollowingVersionAndClustersAlongWithNonce)
-	ctx.Step(`^the Client sends an ACK to which the server does not respond$`, r.TheClientSendsAnACKToWhichTheServerDoesNotRespond)
-    ctx.Step(`^the Target is updated to the following state:$`, r.TheTargetIsUpdatedToTheFollowingState)
     ctx.Step(`^a target setup with "([^"]*)", "([^"]*)", and "([^"]*)"$`, r.ATargetSetupWithServiceResourcesAndVersion)
 	ctx.Step(`^the Client does a wildcard subscription to "([^"]*)"$`, r.TheClientDoesAWildcardSubscriptionToService)
     ctx.Step(`^the Client receives the "([^"]*)" and "([^"]*)" for "([^"]*)"$`, r.TheClientReceivesCorrectResourcesAndVersionForService)
